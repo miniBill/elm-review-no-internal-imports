305196ae9d5ff278c8d806fc9faa2f8a72925700
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-01-04 15:05:45 +0100

    Write some README, some tests

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # elm-review-no-internal-imports
 
-Provides [`elm-review`](https://package.elm-lang.org/packages/jfmengels/elm-review/latest/) rules to REPLACEME.
+Provides [`elm-review`](https://package.elm-lang.org/packages/jfmengels/elm-review/latest/) rules to enforce boundaries in projects that vendor code.
 
 ## Provided rules
 
-- [`NoInternalImports`](https://package.elm-lang.org/packages/miniBill/elm-review-no-internal-imports/1.0.0/NoInternalImports) - Reports REPLACEME.
+- [`NoInternalImports`](https://package.elm-lang.org/packages/miniBill/elm-review-no-internal-imports/1.0.0/NoInternalImports) - Reports boundary violations for projects with vendored code..
 
 ## Configuration
 
@@ -16,9 +16,15 @@ import Review.Rule exposing (Rule)
 
 config : List Rule
 config =
-    [ NoInternalImports.rule
+    [ NoInternalImports.rule []
     ]
 ```
+
+You must pass as parameter all the `source-directories` that are _outside_ your project's root (that is, the ones that start with `..` or `/`).
+
+Package boundaries are automatically detected by searching for `elm.json` files one level up from the directories ending with `/src`.
+
+The packages don't need to be published, so you can separate a larger app into "packages" by simply creating the necessary `elm.json` files. The rule extracts the exposed modules from there and prevents importing of anything else.
 
 ## Try it out
 
--- tests/NoInternalImportsTest.elm
+++ tests/NoInternalImportsTest.elm
@@ -1,6 +1,9 @@
 module NoInternalImportsTest exposing (all)
 
+import Elm.Project
+import Elm.Version as Version
 import NoInternalImports exposing (rule)
+import Review.Project as Project exposing (Project)
 import Review.Test
 import Test exposing (Test, describe, test)
 
@@ -8,24 +11,53 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "NoInternalImports"
-        [ test "should not report an error when REPLACEME" <|
+        [ test "should not report an error when importing an exposed module" <|
             \() ->
                 """module A exposing (..)
+
+import Element
+
 a = 1
 """
-                    |> Review.Test.run (rule [])
+                    |> Review.Test.runWithProjectData project (rule [])
                     |> Review.Test.expectNoErrors
-        , test "should report an error when REPLACEME" <|
-            \() ->
-                """module A exposing (..)
-a = 1
-"""
-                    |> Review.Test.run (rule [])
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error
-                            { message = "REPLACEME"
-                            , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
-                            }
-                        ]
+
+        --         , test "should report an error when importing an internal module" <|
+        --             \() ->
+        --                 """module A exposing (..)
+        -- import Element.Internal
+        -- a = 1
+        -- """
+        --                     |> Review.Test.runWithProjectData project (rule [])
+        --                     |> Review.Test.expectErrors
+        --                         [ Review.Test.error
+        --                             { message = "REPLACEME"
+        --                             , details = [ "REPLACEME" ]
+        --                             , under = "REPLACEME"
+        --                             }
+        --                         ]
         ]
+
+
+project : Project
+project =
+    Project.new
+        |> Project.addElmJson elmJson
+
+
+elmJson : { path : String, raw : String, project : Elm.Project.Project }
+elmJson =
+    { path = "elm.json"
+    , raw = "{}"
+    , project =
+        Elm.Project.Application
+            { depsDirect = []
+            , depsIndirect = []
+            , dirs = []
+            , elm =
+                Version.fromTuple ( 0, 19, 0 )
+                    |> Maybe.withDefault Version.one
+            , testDepsDirect = []
+            , testDepsIndirect = []
+            }
+    }
